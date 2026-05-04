2d42c919de3599d31f5106771c59a51d76493677
temiscripts <mokhefue@gmail.com>
2026-05-04 11:57:25 +0100

    feat: add ALU operations and output MUX with case statement

--- alu/alu.sv
+++ alu/alu.sv
@@ -20,7 +20,37 @@ output logic overflow,
 output logic [31:0] result
 );
 
+logic [32:0] add_result;
+logic [32:0] sub_result;
+logic [31:0] and_result;
+logic [31:0] or_result;
+logic [31:0] xor_result;
+logic [31:0] nand_result;
+logic [31:0] nor_result;
+logic [31:0] not_result;
 
+assign and_result = a & b;
+assign or_result = a | b;
+assign xor_result = a ^ b;
+assign nand_result = ~(a&b);
+assign nor_result = ~(a | b);
+assign not_result = ~a;
 
+assign add_result = {1'b0, a} + {1'b0, b};
+assign sub_result = {1'b0, a} + {1'b0, ~b} + 1;
+
+always_comb begin
+    case (alu_op)
+        ADD :     result = add_result[31:0];
+        SUB :     result = sub_result[31:0];
+        AND_OP  :  result = and_result;
+        OR_OP   :   result = or_result;
+        NAND_OP : result = nand_result;
+        XOR_OP  : result = xor_result;
+        NOR_OP  : result = nor_result;
+        NOT_OP  : result = not_result;
+        default: result = 32'b0;
+    endcase
+end
 
 endmodule
